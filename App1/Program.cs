e59d2e3ecfbd504fdaa0314f8faeb1cdec814203
dmlob <dmlob@DESKTOP-0VEH751>
2022-12-05 22:36:32 +0300

    Добавьте файлы проекта.

---
+++ App1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Документы\Temp";
+            string[] files = Directory.GetFiles(path, "*.txt", SearchOption.AllDirectories);
+            foreach(string file in files)
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ App2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 10;
+            string path = @"C:\Документы\Temp\Numbers.txt";
+            Random random = new Random();
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    sw.WriteLine(random.Next(0, 100));
+                }
+            }
+            int S = 0;
+            using (StreamReader sr = new StreamReader(path))
+            {
+               
+                for (int i = 0; i < n; i++)
+                {
+                    S += Convert.ToInt32(sr.ReadLine());
+                }
+            }
+            Console.WriteLine(S);
+            Console.ReadKey();
+        }
+    }
+}
